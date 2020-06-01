3ca14473247953d5df097f6741e59e69e32fe0f7
root <root@PC.localdomain>
2020-05-31 22:23:07 -0300

    Criado o model de compras

---
+++ src/model/Buy.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Products = require("./Products");
+const Users = require("./Users");
+
+const Buy = connection.define("buy", {
+  date: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  cpf: {
+    type: Sequelize.STRING,
+    references: {
+      model: Users,
+      key: "cpf",
+      deferrable: Sequelize.INITIALLY_IMMEDIATE,
+    },
+  },
+});
+
+Products.hasMany(Buy);
+
+Buy.belongsTo(Products);
+
+Buy.sync({ force: false });
+
+module.exports = Buy;
