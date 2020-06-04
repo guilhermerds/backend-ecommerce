a68a9f834ae398edbdfab6bae8060766b9c7a86c
root <root@PC.localdomain>
2020-06-04 16:51:18 -0300

    Adicionado a rota de usuarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Shipping = require("./public/Shipping");
+const User = require("./user/User");
 
 router.use(Shipping);
+
+router.use(User);
 
 module.exports = router;
