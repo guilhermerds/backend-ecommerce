7a14de9c5638585088087cc7f048205c691d5c59
root <root@PC.localdomain>
2020-06-04 16:56:34 -0300

    Adicionado a rota de login

--- src/routes/user/User.js
+++ src/routes/user/User.js
@@ -4,4 +4,6 @@ const UserController = require("../../controller/User");
 
 router.post("/user/new", UserController.create);
 
+router.post("/user/login", UserController.login);
+
 module.exports = router;
