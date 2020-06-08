62f3688c09dd81e87de92545b47a63819742ee24
root <root@PC.localdomain>
2020-06-07 23:18:09 -0300

    Criado o arquivo de controle de produtos com a listagem deles

---
+++ src/controller/Product.js
@@ -0,0 +1,40 @@
+const Product = require("../model/Products");
+
+module.exports = {
+  async index(req, res) {
+    const { page } = req.params;
+    const limit = 20;
+    let offset = 0;
+
+    if (!isNaN(page) && page >= 0) {
+      offset = (page - 1) * limit;
+    }
+
+    await Product.findAndCountAll({
+      limit,
+      offset,
+    })
+      .then((products) => {
+        let next = false;
+        if (products.count > 0) {
+          if (products.count > limit + offset) {
+            next = true;
+          }
+
+          res.json({ error: false, next, products });
+        } else {
+          res.json({
+            error: true,
+            msg:
+              "Não possuimos produtos com essas características, tente de outro jeito",
+          });
+        }
+      })
+      .catch(() => {
+        res.json({
+          error: true,
+          msg: "Ocorreu um problema, tente novamente mais tarde",
+        });
+      });
+  },
+};
