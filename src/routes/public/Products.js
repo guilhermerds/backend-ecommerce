54206d33035de0541c22bfc0f0cb832985dd0362
root <root@PC.localdomain>
2020-06-11 22:18:01 -0300

    Coloquei o params

--- src/routes/public/Products.js
+++ src/routes/public/Products.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ProductsController = require("../../controller/Product");
 
-router.get("/products", ProductsController.index);
+router.get("/products/:page", ProductsController.index);
 
 module.exports = router;
