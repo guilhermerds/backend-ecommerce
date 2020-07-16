361a3e4857071436fe3c12eb7f6dd0ff98761feb
root <root@PC.localdomain>
2020-07-16 14:53:23 -0300

    Trocado o metodo da requisicao

--- src/routes/user/Buy.js
+++ src/routes/user/Buy.js
@@ -4,6 +4,6 @@ const UserBuy = require("../../controller/UserBuy");
 const Middleware = require("../middleware/authorize");
 
 router.post("/buy", Middleware.user, UserBuy.create);
-router.post("/buy/list", Middleware.user, UserBuy.show);
+router.get("/buy", Middleware.user, UserBuy.show);
 
 module.exports = router;
