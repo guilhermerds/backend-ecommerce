499b1748c310ad4c799e7a163f8194fe7ba12909
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2020-07-17 15:08:42 -0300

    Adicionado a rota de login do employee

--- src/routes/employee/Employee.js
+++ src/routes/employee/Employee.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const EmployeeController = require("../../controller/Employee");
 
 router.post("/employee/new", EmployeeController.create);
+router.post("/employee/login", EmployeeController.login);
 
 module.exports = router;
