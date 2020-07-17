ef0440f56abaf1266a2b3105aebb4019dc27b366
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2020-07-17 15:49:05 -0300

    Adicionado atualização de pedido

--- src/controller/EmployeeBuy.js
+++ src/controller/EmployeeBuy.js
@@ -20,4 +20,41 @@ module.exports = {
         });
       });
   },
+  update(req, res) {
+    const { id, statusId } = req.body;
+
+    if (
+      (id == undefined && !isNaN(id)) ||
+      (statusId == undefined && !isNaN(statusId))
+    ) {
+      return res.json({
+        error: true,
+        msg: "Informe a identificação da compra e seu status",
+      });
+    }
+
+    let status;
+
+    switch (statusId) {
+      case 1:
+        status = "Pedido separado para envio";
+        break;
+      case 2:
+        status = "Pedido enviado";
+        break;
+      default:
+        return res.json({
+          error: true,
+          msg: "Código de status inválido, tente outro",
+        });
+    }
+
+    Buy.update({ status }, { where: { id } })
+      .then(() => {
+        return res.json({ error: false, msg: "Atualizado com sucesso" });
+      })
+      .catch(() => {
+        return res.json({ error: true, msg: "Ocorreu um erro na atualização" });
+      });
+  },
 };
--- src/routes/employee/Buy.js
+++ src/routes/employee/Buy.js
@@ -4,5 +4,6 @@ const EmployeeBuyController = require("../../controller/EmployeeBuy");
 const Middleware = require("../middleware/authorize");
 
 router.get("/buy/employee", Middleware.employee, EmployeeBuyController.show);
+router.put("/buy/employee", Middleware.employee, EmployeeBuyController.update);
 
 module.exports = router;
